69b59143f52cec9d069aaa7f1459fd726d287369
Matt Hughes <hughesm02@gmail.com>
2026-03-01 18:16:11 -0800

    Cleanly exit PixelAt if image is in zombie state

--- src/image.h
+++ src/image.h
@@ -12,6 +12,29 @@ struct ImageData
     int height = 0;
     std::vector<float> pixels; // RGBA interleaved, width*height*4 floats
 
+    ImageData() = default;
+    ImageData(const ImageData&) = default;
+    ImageData& operator=(const ImageData&) = default;
+
+    ImageData(ImageData&& o) noexcept : width(o.width), height(o.height), pixels(std::move(o.pixels))
+    {
+        o.width = 0;
+        o.height = 0;
+    }
+
+    ImageData& operator=(ImageData&& o) noexcept
+    {
+        if (this != &o)
+        {
+            width = o.width;
+            height = o.height;
+            pixels = std::move(o.pixels);
+            o.width = 0;
+            o.height = 0;
+        }
+        return *this;
+    }
+
     const float* PixelAt(int x, int y) const { return &pixels[static_cast<size_t>((y * width + x) * 4)]; }
 
     bool IsLoaded() const { return width > 0 && height > 0; }
